7e80a53bf8f374248852dfc3c38811d549f909b9
Arnold <62089140+central182@users.noreply.github.com>
2021-02-27 21:19:23 +0900

    Implement deserializer

--- deserializer.js
+++ deserializer.js
@@ -10,6 +10,38 @@ function decode(value) {
   if ("stringValue" in value) {
     return value.stringValue;
   }
+  if ("doubleValue" in value) {
+    return value.doubleValue;
+  }
+  if ("integerValue" in value) {
+    return parseInt(value.integerValue);
+  }
+  if ("booleanValue" in value) {
+    return value.booleanValue;
+  }
+  if ("nullValue" in value) {
+    return null;
+  }
+  // TODO: use firestore Timestamp type
+  if ("timestampValue" in value) {
+    return value.timestampValue;
+  }
+  // TODO: use firestore GeoPoint type
+  if ("geoPointValue" in value) {
+    return value.geoPointValue;
+  }
+  // TODO: use firestore DocumentReference type
+  if ("referenceValue" in value) {
+    return value.referenceValue;
+  }
+  // TODO: when is bytesValue used?
+  if ("arrayValue" in value) {
+    return value.arrayValue.values.map((value) => decode(value));
+  }
+  if ("mapValue" in value) {
+    return deserialize(value.mapValue.fields);
+  }
+  return null;
 }
 
 module.exports = deserialize;
