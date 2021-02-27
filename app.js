6ec74792ade66cad24dce3cd363e500963b4bedc
Arnold <62089140+central182@users.noreply.github.com>
2021-02-27 23:18:12 +0900

    Remove unnecessary files

--- app.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-const deserialize = require("./deserializer");
-const serialize = require("./serializer");
-
-const source = JSON.parse(require("fs").readFileSync("source.json", "utf-8"));
-const obj = deserialize(source);
-console.log(JSON.stringify(obj, null, 2));
